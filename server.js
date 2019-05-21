d5234a6c396f8ebde26092d09e07fa9d1f3ca2ca
lioartoil <a.chanakant@gmail.com>
2019-05-21 21:05:28 +0700

    full project

--- server.js
+++ server.js
@@ -6,27 +6,35 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function(req, res) {
+	res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/timestamp/:date_string', ({ params: { date_string } }, res) => {
+	let date = new Date();
 
+	if (!date_string) date = new Date();
+	else if (date_string.split('-').length === 3) date = new Date(date_string);
+	else if (parseInt(date_string) >= 0) date = new Date(parseInt(date_string));
+	else return res.json({ error: 'Invalid Date' });
+
+	res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+	console.log('Your app is listening on port ' + listener.address().port);
 });
